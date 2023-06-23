e2ebbe820da213ec936c06347d282cdd7203f47b
harshedabdulla <harshed2004@gmail.com>
2023-06-23 17:20:23 +0530

    changes

---
+++ pr-4/react-chatgpt/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <section className='side-bar'>
+        <button> + New chat</button>
+        <ul className='history'></ul>
+        <nav>
+          <p>Made by Ania</p>
+        </nav>
+      </section>
+
+    </div>
+  )
+}
+
+export default App
