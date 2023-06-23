2b99e1ff7568286c3747dc9352957f59305dda2e
harshedabdulla <harshed2004@gmail.com>
2023-06-23 17:58:48 +0530

    completed till seting up frontend

--- pr-4/react-chatgpt/src/App.js
+++ pr-4/react-chatgpt/src/App.js
@@ -5,10 +5,29 @@ const App = () => {
     <div className='app'>
       <section className='side-bar'>
         <button> + New chat</button>
-        <ul className='history'></ul>
+        <ul className='history'>
+          <li>Blugh</li>
+        </ul>
         <nav>
           <p>Made by Ania</p>
         </nav>
+      </section>
+      <section className='main'>
+        <h1>HarshedGPT</h1>
+        <ul className='feed'>
+
+        </ul>
+        <div className='bottom-section'>
+          <div className='input-container'>
+            <input/>
+            <div id='submit'>➤</div>
+          </div>
+          <p className='info'>
+            Chat GPT Mar 14 Version. Free Research Preview.
+            our goal is to make AI systems more natural and safe to interact with.
+            Your feedback will help us improve.
+          </p>
+        </div>
       </section>
 
     </div>
